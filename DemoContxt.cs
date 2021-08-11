fb8ce94afa3281a2818eda413675a0506ec7b325
culmencasa <culmencasa@gmail.com>
2021-08-11 13:43:33 +0800

    窗体关闭退出程序

--- DemoContxt.cs
+++ DemoContxt.cs
@@ -8,11 +8,30 @@ namespace DpiScaleFormDemo
 {
     class DemoContxt : ApplicationContext
     {
+        Queue<int> forms = new Queue<int>();
+
         public DemoContxt()
         {
-            new Form1().Show();
+            var form1 = new Form1();
+            var form2 = new Form2();
+            forms.Enqueue(1);
+            forms.Enqueue(2);
 
-            new Form2().Show();
+            form1.FormClosed += Form_FormClosed;
+            form2.FormClosed += Form_FormClosed;
+
+            form1.Show();
+            form2.Show();
+
+        }
+
+        private void Form_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            forms.Dequeue();
+            if (forms.Count == 0)
+            {
+                ExitThread();
+            }
         }
     }
 }
